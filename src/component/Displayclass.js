c2e0f51a745c2833d3476e839b6c4012bc7c48f5
Dabloo-9123 <dablook2000@gmail.com>
2023-07-24 01:33:16 +0530

    commit

---
+++ src/component/Displayclass.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import '../App'
+
+class Displayclass extends Component{
+    constructor(props){
+        super(props);
+        console.log(this.props.value);
+    }
+render(){
+    return(
+        <>
+           
+            <h1>EMPLOYEE FEEDBACK DATA</h1>
+            <div className="parent_box">
+            {this.props.value.map((item,index)=>{
+                return(
+                    <div className="content_box">
+                    <span key={index}>Name:{item.name}|Department:{item.dept}|Rating:{item.rate}</span>
+                    </div>
+                )
+            })}
+                <br/>
+            
+            </div>
+            <div className="btn" >
+            <button onClick={this.props.togglefun} >Go back</button>
+            </div>
+            
+        </>
+    )
+}
+
+}
+export default Displayclass;
